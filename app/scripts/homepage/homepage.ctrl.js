518ea3875357038a11c472bd046b6db5d5dfc94d
Horsed <legendaryhorsed@gmail.com>
2014-09-30 22:46:45 +0200

    dummy controller now uses angular dependency literals

--- app/scripts/homepage/homepage.ctrl.js
+++ app/scripts/homepage/homepage.ctrl.js
@@ -1,10 +1,10 @@
 'use strict';
 
 angular.module('myApp.homepage')
-  .controller('HomepageCtrl', function ($scope) {
+  .controller('HomepageCtrl', ['$scope', function ($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-  });
+  }]);
